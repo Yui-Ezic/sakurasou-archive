5108329a9dd951ff6e058aab99471f9c734164ae
YuiEzic <mishan221199@gmail.com>
2023-02-11 16:51:44 +0200

    chore: fix codestyle

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Auth\Entity\User;
 
 interface UserRepository
 {
     public function add(User $user): void;
+
     public function hasByUserName(UserName $userName): bool;
 }
